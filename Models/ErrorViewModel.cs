74c5c4e9a971642ca2287ff29bd08291665cd7d9
UAzam <58387873+UAzam@users.noreply.github.com>
2021-04-20 23:50:26 +0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IStoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
